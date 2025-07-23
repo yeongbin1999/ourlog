f8327b3046cecb60e81acb7f5abe3476723c6db3
yeongbin1999 <yeongbin1999@gmail.com>
2025-07-23 16:26:10 +0900

    infra: 초기 셋팅

---
+++ backend/src/test/java/com/back/ourlog/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back.ourlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
