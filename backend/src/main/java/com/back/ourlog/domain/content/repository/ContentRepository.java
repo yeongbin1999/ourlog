6eeafa89f7321d4130123d478cc0a9725793d3fa
jueunk617 <juuuune62@naver.com>
2025-07-25 11:18:01 +0900

    chore(be): dev 브 치 병합 및 Repository 충돌 해결 (#33)

--- backend/src/main/java/com/back/ourlog/domain/content/repository/ContentRepository.java
+++ backend/src/main/java/com/back/ourlog/domain/content/repository/ContentRepository.java
@@ -1,18 +1,10 @@
 package com.back.ourlog.domain.content.repository;
 
 import com.back.ourlog.domain.content.entity.Content;
-<<<<<<< HEAD
 import com.back.ourlog.domain.content.entity.ContentType;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
 
 public interface ContentRepository extends JpaRepository<Content, Integer> {
     Optional<Content> findByExternalIdAndType(String externalId, ContentType type);
-=======
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ContentRepository extends JpaRepository<Content, Integer> {
-
->>>>>>> origin/dev
 }
