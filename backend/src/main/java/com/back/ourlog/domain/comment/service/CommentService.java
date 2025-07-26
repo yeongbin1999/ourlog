6e8bdb3fdb69619778ee15b90d67ca7ff46904b6
namgigun <namgigun@gmail.com>
2025-07-26 15:51:28 +0900

    Refactor: 댓글 작성 관련 코드 리팩토링
    기존 -> CommentService안에서 Comment 객체를 만들어서 기존 Diary 객체 안에 Comment를 직접 삽입하는 방식
    변경 -> Diary에 addComment 메서드로 댓글 정보를 넣으면 Diary 내에서 Comment를 만들어서 추가하는 방식

--- backend/src/main/java/com/back/ourlog/domain/comment/service/CommentService.java
+++ backend/src/main/java/com/back/ourlog/domain/comment/service/CommentService.java
@@ -1,7 +1,6 @@
 package com.back.ourlog.domain.comment.service;
 
 import com.back.ourlog.domain.comment.entity.Comment;
-import com.back.ourlog.domain.comment.repository.CommentRepository;
 import com.back.ourlog.domain.diary.entity.Diary;
 import com.back.ourlog.domain.diary.repository.DiaryRepository;
 import com.back.ourlog.domain.user.entity.User;
@@ -12,14 +11,15 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @RequiredArgsConstructor
 public class CommentService {
-    private final CommentRepository commentRepository;
     private final DiaryRepository diaryRepository;
+
     @Transactional
     public Comment write(int diaryId, User user, String content) {
         Diary diary = diaryRepository.findById(diaryId).orElseThrow();
-        // 테스트 ver (원래는 user가 있다고 가정)
-        Comment comment = new Comment(diary,null, content);
+        return diary.addComment(user, content);
+    }
 
-        return commentRepository.save(comment);
+    public void flush() {
+        diaryRepository.flush();
     }
 }
