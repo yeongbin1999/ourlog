f2ace32907a52cd1fa0e8ab4cebb46e371e8e1d5
namgigun <namgigun@gmail.com>
2025-07-28 22:44:11 +0900

    Feat: Spotify & TMDB API 연동 Merge

--- backend/src/main/java/com/back/ourlog/domain/content/controller/ContentController.java
+++ backend/src/main/java/com/back/ourlog/domain/content/controller/ContentController.java
@@ -1,19 +1,31 @@
 package com.back.ourlog.domain.content.controller;
 
 import com.back.ourlog.domain.content.dto.ContentResponseDto;
+import com.back.ourlog.domain.content.dto.LibraryApiResponseDto;
 import com.back.ourlog.domain.content.service.ContentService;
 import com.back.ourlog.global.rsData.RsData;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.List;
+import java.util.Map;
 
 @RestController
-@RequestMapping("/api/v1/contents")
+@RequestMapping(value = "/api/v1/contents")
 @RequiredArgsConstructor
+@Log4j2
 public class ContentController {
     private final ContentService contentService;
     @GetMapping("/{diaryId}")
@@ -22,5 +34,46 @@ public class ContentController {
 
         return ResponseEntity.status(HttpStatus.OK)
                 .body(RsData.of("200-1", "%d번 다이어리의 조회 컨텐츠가 조회되었습니다.".formatted(diaryId), res));
+    }
+
+    @GetMapping("/store")
+    public List<LibraryApiResponseDto> callLibraryApi() throws Exception {
+        RestTemplate restTemplate = new RestTemplate();
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        String apiKey = "61b675fb2f116a198116c3291a3070cc8b7d355d1dcdbc8d4f0da6f17c3d62d8";
+
+        String url = "https://www.nl.go.kr/seoji/SearchApi.do?cert_key=%s".formatted(apiKey) +
+                "&result_style=json&page_no=1&page_size=100";
+
+        String data = restTemplate.getForObject(url, String.class);
+
+        Map map = objectMapper.readValue(data, Map.class);
+
+        List<Map<String, Object>> result = (List<Map<String, Object>>) map.get("docs");
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+
+        List<LibraryApiResponseDto> res = result.stream()
+                .map(item -> {
+                    String title = (String) item.get("TITLE");
+                    String description = (String) item.get("BOOK_INTRODUCTION");
+                    String posterUrl = (String) item.get("TITLE_URL");
+                    String releasedAtStr = (String) item.get("PUBLISH_PREDATE");
+
+                    LocalDateTime releasedAt = null;
+                    if (releasedAtStr != null && !releasedAtStr.isBlank()) {
+                        try {
+                            releasedAt = LocalDate.parse(releasedAtStr, formatter).atStartOfDay();
+                        } catch (DateTimeParseException e) {
+                            log.info("날짜 파싱 실패");
+                        }
+                    }
+
+                    return new LibraryApiResponseDto(title, description, posterUrl, releasedAt);
+                })
+                .toList();
+
+        return res;
     }
 }
---
+++ backend/src/main/java/com/back/ourlog/domain/content/dto/LibraryApiResponseDto.java
@@ -0,0 +1,25 @@
+package com.back.ourlog.domain.content.dto;
+
+import com.back.ourlog.domain.content.entity.ContentType;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class LibraryApiResponseDto {
+    private String title;
+    private ContentType contentType;
+
+    private String description;
+    private String posterUrl;
+
+    private LocalDateTime releasedAt;
+
+    public LibraryApiResponseDto(String title, String description, String posterUrl, LocalDateTime releasedAt) {
+        this.title = title;
+        contentType = ContentType.BOOK;
+        this.description = description;
+        this.posterUrl = posterUrl;
+        this.releasedAt = releasedAt;
+    }
+}
