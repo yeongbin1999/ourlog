52c1354f4566a5df4441161f2808b77501f41fee
NOHHEECHEOL <127588192+Nohheechul@users.noreply.github.com>
2025-07-29 12:06:41 +0900

    feat(be): 타입별 그래프 데이터 조회 API 구현 (#86)

--- backend/src/test/java/com/back/ourlog/domain/statistics/contoroller/StatisticsControllerTest.java
+++ backend/src/test/java/com/back/ourlog/domain/statistics/contoroller/StatisticsControllerTest.java
@@ -1,9 +1,8 @@
 package com.back.ourlog.domain.statistics.contoroller;
 
 import com.back.ourlog.domain.statistics.controller.StatisticsController;
-import com.back.ourlog.domain.statistics.dto.MonthlyDiaryCount;
-import com.back.ourlog.domain.statistics.dto.StatisticsCardDto;
-import com.back.ourlog.domain.statistics.dto.TypeCountDto;
+import com.back.ourlog.domain.statistics.dto.*;
+import com.back.ourlog.domain.statistics.enums.PeriodOption;
 import com.back.ourlog.domain.statistics.service.StatisticsService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -115,5 +114,43 @@ public class StatisticsControllerTest {
                     .andExpect(jsonPath("$.[%d].type".formatted(i)).value(typeCountDto.getType()))
                     .andExpect(jsonPath("$.[%d].count".formatted(i)).value(typeCountDto.getCount()));
         }
+    }
+
+    @Test
+    @DisplayName("타입 그래프 조회")
+    void 타입_그래프() throws Exception {
+
+        ResultActions resultActions = mvc.perform(
+                get("/api/v1/statistics/type-graph")
+                .param("userId", "1")
+                .param("period", "ALL")  // 예시로 MONTH 기간을 사용
+        ).andDo(print());
+
+        resultActions
+                .andExpect(handler().handlerType(StatisticsController.class))
+                .andExpect(handler().methodName("getTypeGraph"))
+                .andExpect(status().isOk());
+
+        TypeGraphResponse typeGraphResponse = statisticsService.getTypeGraph(new TypeGraphRequest(1, PeriodOption.ALL));
+
+        List<TypeLineGraphDto> trend = typeGraphResponse.getTypeLineGraph();
+        List<TypeRankDto> ranking = typeGraphResponse.getTypeRanking();
+
+        for(int i = 0; i < trend.size(); i++) {
+            TypeLineGraphDto typeLineGraphDto = trend.get(i);
+            System.out.println("Axis Label: " + typeLineGraphDto.getAxisLabel() + ", Type: " + typeLineGraphDto.getType() + ", Count: " + typeLineGraphDto.getCount());
+            resultActions
+                    .andExpect(jsonPath("$.typeLineGraph.[%d].axisLabel".formatted(i)).value(typeLineGraphDto.getAxisLabel()))
+                    .andExpect(jsonPath("$.typeLineGraph.[%d].type".formatted(i)).value(typeLineGraphDto.getType().name()))
+                    .andExpect(jsonPath("$.typeLineGraph.[%d].count".formatted(i)).value(typeLineGraphDto.getCount()));
+        }
+        for(int i = 0; i < ranking.size(); i++) {
+            TypeRankDto typeRankDto = ranking.get(i);
+            System.out.println("Type: " + typeRankDto.getType() + ", Count: " + typeRankDto.getTotalCount());
+            resultActions
+                    .andExpect(jsonPath("$.typeRanking.[%d].type".formatted(i)).value(typeRankDto.getType().name()))
+                    .andExpect(jsonPath("$.typeRanking.[%d].totalCount".formatted(i)).value(typeRankDto.getTotalCount()));
+        }
+
     }
 }
