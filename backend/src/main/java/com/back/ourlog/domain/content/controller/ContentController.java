14720aeb8bc7dbf9784c1148e32956f36f73e3f2
yeongbin1999 <yeongbin1999@gmail.com>
2025-07-29 17:38:41 +0900

    refactor(be)

--- backend/src/main/java/com/back/ourlog/domain/content/controller/ContentController.java
+++ backend/src/main/java/com/back/ourlog/domain/content/controller/ContentController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/api/v1/contents")
+@RequestMapping("/api/v1/contents")
 @RequiredArgsConstructor
 @Log4j2
 public class ContentController {
