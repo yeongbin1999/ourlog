4936c2ea45e20865ac539944fe8400226390a17e
namgigun <namgigun@gmail.com>
2025-07-29 16:52:14 +0900

    Feat: .env 환경변수 적용

--- backend/src/main/java/com/back/ourlog/domain/content/controller/ContentController.java
+++ backend/src/main/java/com/back/ourlog/domain/content/controller/ContentController.java
@@ -7,6 +7,7 @@ import com.back.ourlog.global.rsData.RsData;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -28,6 +29,9 @@ import java.util.Map;
 @Log4j2
 public class ContentController {
     private final ContentService contentService;
+    @Value("${library.api-key}")
+    private String apiKey;
+
     @GetMapping("/{diaryId}")
     public ResponseEntity<RsData<ContentResponseDto>> getContent(@PathVariable("diaryId") int diaryId) {
         ContentResponseDto res = contentService.getContent(diaryId);
@@ -40,8 +44,6 @@ public class ContentController {
     public ResponseEntity<RsData<List<LibraryApiResponseDto>>> callLibraryApi() throws Exception {
         RestTemplate restTemplate = new RestTemplate();
         ObjectMapper objectMapper = new ObjectMapper();
-
-        String apiKey = "61b675fb2f116a198116c3291a3070cc8b7d355d1dcdbc8d4f0da6f17c3d62d8";
 
         String url = "https://www.nl.go.kr/seoji/SearchApi.do?cert_key=%s".formatted(apiKey) +
                 "&result_style=json&page_no=1&page_size=100";
