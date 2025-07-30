1642393eca262f82868a409e671938b5b772918f
namgigun <namgigun@gmail.com>
2025-07-30 16:06:39 +0900

    Fix: 내용이 없어도 댓글 추가되는 문제 해결
    댓글 입력 폼에서 아무 내용을 입력하지 않으면 "댓글 내용을 입력해주세요." 팝업창이 나타나도록 함

--- frontend/src/app/diary/[id]/page.tsx
+++ frontend/src/app/diary/[id]/page.tsx
@@ -88,6 +88,11 @@ function CommentForm({
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
+    if (!content.trim()) {
+      alert("댓글 내용을 입력해주세요");
+      return;
+    }
+
     try {
       const response = await fetch("http://localhost:8080/api/v1/comments", {
         method: "POST",
@@ -106,7 +111,7 @@ function CommentForm({
       // 입력 초기화
       setContent("");
       console.log(result.data);
-      // 부모 상태 업데이트
+      // 상태 업데이트
       onCommentAdd(result.data);
 
       alert("댓글 등록에 성공하였습니다.");
@@ -117,31 +122,33 @@ function CommentForm({
   };
 
   return (
-    <section className="p-6 border rounded-xl shadow-sm bg-white space-y-4">
-      <form className="flex flex-col gap-3" onSubmit={handleSubmit}>
-        <label className="text-sm text-gray-600">Nickname</label>
-        <textarea
-          className="border p-2 rounded-md h-24 resize-none"
-          name="content"
-          placeholder="댓글을 입력하세요"
-          value={content}
-          onChange={(e) => setContent(e.target.value)}
-        />
-        <button
-          type="submit"
-          className="self-end px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
-        >
-          등록
-        </button>
-      </form>
-    </section>
+    <>
+      <h2 className="text-xl font-semibold">댓글</h2>
+      <section className="p-6 border rounded-xl shadow-sm bg-white space-y-4">
+        <form className="flex flex-col gap-3" onSubmit={handleSubmit}>
+          <label className="text-sm text-gray-600">Nickname</label>
+          <textarea
+            className="border p-2 rounded-md h-24 resize-none"
+            name="content"
+            placeholder="댓글을 입력하세요"
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+          />
+          <button
+            type="submit"
+            className="self-end px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
+          >
+            등록
+          </button>
+        </form>
+      </section>
+    </>
   );
 }
 
 function CommentInfo({ comments }: { comments: Comment[] }) {
   return (
     <section className="space-y-4">
-      <h2 className="text-xl font-semibold">댓글</h2>
       {comments.length === 0 ? (
         <p className="text-gray-500">등록된 댓글이 없습니다.</p>
       ) : (
