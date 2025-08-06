aa389b755a8f2f8305b8e7cd389f3d9685251f4c
yeongbin1999 <yeongbin1999@gmail.com>
2025-08-06 23:48:52 +0900

    feat(be): web cors 추가 설정

--- backend/src/main/java/com/back/ourlog/global/config/WebConfig.java
+++ backend/src/main/java/com/back/ourlog/global/config/WebConfig.java
@@ -14,7 +14,8 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins(
                         "http://localhost:3000",
                         "http://127.0.0.1:3000",
-                        "https://ourlog.vercel.app"
+                        "https://ourlog.vercel.app",
+                        "https://ourlog.shop"
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
